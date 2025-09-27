2967aced2d7b33419a9224a685f81e9b9a6dfcfd
caoquanbao <cuchodayh1@gmail.com>
2025-09-27 23:54:51 +0700

    Delete Private_Project/EcommerceProject directory

--- Private_Project/EcommerceProject/Ecommerce_Demo/demo/demo/src/main/java/com/example/demo/model/ProductCategory.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.model;
-
-public enum ProductCategory {
-    ELECTRONICS,
-    CLOTHING,
-    BOOKS,
-    TOYS
-}
--- Private_Project/EcommerceProject/Ecommerce_Demo/demo/demo/src/main/java/com/example/demo/model/Status.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model;
-
-public enum Status {
-    PENDING,     // Chờ xử lý
-    CONFIRMED,   // Đã xác nhận
-    SHIPPED,     // Đang giao
-    COMPLETED,   // Hoàn tất
-    CANCELLED    // Hủy
-}
